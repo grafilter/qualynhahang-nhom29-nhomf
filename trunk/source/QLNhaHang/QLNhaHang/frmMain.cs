4adee21c4b83c5d966604491d5be7a0d00abae59
leenoyu <leenoyu@gmail.com>
2013-05-30 07:07:15 +0000

    Tạo project Tạo Menu frmMain Lê Ngọc Vũ

---
+++ trunk/source/QLNhaHang/QLNhaHang/frmMain.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevComponents.DotNetBar.Metro;
+
+namespace QLNhaHang
+{
+    public partial class frmMain : DevComponents.DotNetBar.Metro.MetroAppForm
+    {
+      
+        public frmMain()
+        {
+            InitializeComponent();
+           
+        }      
+
+
+    }
+}
